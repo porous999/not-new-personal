50ea4ba4962b6e38030219b5e4bdca9828032514
Tanmay Saha <tsaha@juniper.net>
2022-09-29 09:53:14 +0530

    Add APP gateway

---
+++ appgateway/src/main/java/com/amazingbooks/appgateway/AppgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.amazingbooks.appgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppgatewayApplication.class, args);
+	}
+
+}
---
+++ appgateway/src/test/java/com/amazingbooks/appgateway/AppgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazingbooks.appgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
